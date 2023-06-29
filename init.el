f4c734fc0e38a45f0373eb30060f639f30786316
Jai <jkbhagatio@gmail.com>
2023-06-29 02:10:42 +0100

    updated with package set up

--- init.el
+++ init.el
@@ -1,14 +1,70 @@
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+(load-theme 'wombat)
+
+;; Enable windmove `shift + <arrow>` keybindings
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+;; Set some UI elements
+(tool-bar-mode -1)     ; disable tool-bar
+(setq visible-bell t)  ; set visible bell (disable audio bell)
+
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents  
+ (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(require 'command-log-mode)
+(use-package command-log-mode)  ; log commands into a buffer: `M-x clm/open-command-log-buffer`
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  :init (ivy-mode 1))
+
+(use-package counsel)
+(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(deeper-blue)))
+ '(package-selected-packages '(doom-modeline counsel ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(setq user-emacs-directory "C:/Users/jai/.emacs.d/")
-(load (expand-file-name ".emacs" user-emacs-directory))
