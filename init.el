bfdcde5b59d90b57a866eb356c195fd01ae67754
Jai <jkbhagatio@gmail.com>
2023-07-01 18:21:06 +0100

    Added some base config history saving and magit to config

--- init.el
+++ init.el
@@ -56,12 +56,14 @@
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-switch-buffer)
+	 ("C-x C-i" . ibuffer)
 	 ("C-x C-f" . counsel-find-file)
 	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history)))
+	 ("C-r" . counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil))  ; don't start searches with `^`
 
 (use-package all-the-icons)  ; Run at new config: M-x all-the-icons-install-fonts
-
 
 (use-package doom-modeline
   :ensure t
@@ -89,13 +91,51 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
+
+(use-package ace-window
+  :bind ("M-o" . ace-window)
+  :config
+  (setq aw-dispatch-always t))
+
+;; Ripgrep
+(use-package wgrep)
+(use-package transient)
+(use-package rg
+  :bind ("C-c r" . rg))
+
+(use-package magit
+  :custom
+  ;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; Turn on recent file mode
+(recentf-mode 1)
+(global-set-key (kbd "C-c C-f") 'recentf-open-files)
+
+;; Turn on minibuffer history
+(setq history-length 25)
+(savehist-mode 1)
+
+;; Turn on save place mode for navigating in place to reopened file
+(save-place-mode 1)
+
+;; Set a location for customization variables so they don't get set here
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
+;; Update files in buffers when they've been changed outside of Emacs
+(global-auto-revert-mode 1)
+(setq global-auto-revert-non-file-buffers t)
+
+;; Use minibuffer area over pop up UI dialogs for some Emacs prompts
+;(setq use-dialog-box nil)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons doom-themes helpful multiple-cursors which-key use-package ivy-rich doom-modeline counsel command-log-mode)))
+   '(magit which-key use-package rg multiple-cursors ivy-rich helpful doom-themes doom-modeline counsel command-log-mode all-the-icons ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
