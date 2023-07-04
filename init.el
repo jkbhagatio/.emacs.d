ac7d07a37f0e7f61dbcdad0dbf47df56a69c3a71
Jai <jkbhagatio@gmail.com>
2023-07-04 02:33:32 +0100

    Configured dired and dirvish...kinda

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (load-theme 'wombat)
 
 ;; Set some UI elements and settings
-(menu-bar-mode -1)                         ; disable menu bar
+;(menu-bar-mode -1)                         ; disable menu bar
 (tool-bar-mode -1)                         ; disable tool bar
 (setq visible-bell t)                      ; set visible bell (disable audio bell)
 (set-fringe-mode '(10 . 10))               ; add some pixels to left and right edge of screen
@@ -37,6 +37,9 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
+;; Keybind commenting
+(global-set-key (kbd "C-x /") 'comment-or-uncomment-region)
+
 ;; Initialize package sources
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -51,6 +54,7 @@
 (setq use-package-always-ensure t)
 
 (use-package command-log-mode)  ; log commands into a buffer: `M-x clm/open-command-log-buffer`
+(setq global-command-log-mode t)
 
 (use-package ivy
   :diminish
@@ -83,7 +87,8 @@
   :init
   (ivy-rich-mode 1))
 
-(use-package all-the-icons)  ; Run at new config: M-x all-the-icons-install-fonts
+					;(use-package all-the-icons)  ; Run at new config: M-x all-the-icons-install-fonts
+(use-package vscode-icon)
 
 (use-package which-key
   :init (which-key-mode)
@@ -104,24 +109,44 @@
 (setq dired-recursive-deletes 'always)
 (setq dired-listing-switches  "-agho --group-directories-first --time-style=long-iso")
 (setq dired-dwim-target t)
-(use-package all-the-icons-dired
-  :hook (dired-mode . all-the-icons-dired-mode))
-(use-package dired-hide-dotfiles ; hide dot files hook in dired
-  :hook (dired-mode . dired-hide-dotfiles-mode))
-(define-key dired-mode-map (kbd "z") 'dired-hide-dotfiles-mode)
-;; Initialize without hiding anything
-(setq dired-hode-dotfiles-mode -1)
-(setq dired-hide-details-mode -1)
+(use-package dired-hacks-utils)
+(use-package dired-hide-dotfiles) ; hide dot files hook in dired
 ;; (use-package dired-open
 ;;   :config
 ;;   (setq dired-open-extensions '(("png" . "feh")
 ;;                                 ("mkv" . "mpv"))))
-;; (use-package dirvish)
-;; (dirvish-override-dired-mode)
-;; (dirvish-override-dired-jump)
-;; (setq dirvish-enable-preview t)
+(use-package pdf-tools)
+(use-package dirvish
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "/c/Users/jai"                   "home")
+     ("g" "/d/google_drive"                "g_drive")))
+  :config
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  (setq dirvish-mode-line-format
+        '(:left (sort symlink) :right (omit yank index file-size file-time)))
+  (setq dirvish-attributes
+	'(file-time file-size collapse subtree-state vc-state git-msg))
+       ;'(vscode-icon file-time file-size collapse subtree-state vc-state git-msg))
+  (setq delete-by-moving-to-trash t)
+  (setq dired-listing-switches
+        "-l --almost-all --human-readable --group-directories-first --no-group")
+  (setq dirvish-enable-preview t)
+  (setq dirvish-use-mode-line t)
+  (setq dirvish-hide-details nil))
+(global-set-key (kbd "C-x M-d") 'dirvish)
+(global-set-key (kbd "C-x M-s") 'dirvish-side)
 ;; (require 'dirvish-minibuffer-preview)
 ;; (dirvish-minibuf-preview-mode)
+;; Initialize without hiding anything
+(setq dired-hode-dotfiles-mode -1)
+(setq dired-hide-details-mode -1)
+(define-key dired-mode-map (kbd "z") 'dired-hide-dotfiles-mode)
+(define-key dired-mode-map (kbd "b") 'dired-hide-details-mode)
+(define-key dired-mode-map (kbd "% C-g") 'dired-do-query-replace-regexp)
 
 (use-package helpful
   :custom
